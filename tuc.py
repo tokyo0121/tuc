626cd0ccbb21c90b5b1c910a16c44aed3107871b
tokyo121 <p19026is@gm.tsuda.ac.jp>
2022-05-21 00:35:51 +0900

    Co-authored-by: NT-TU <NT-TU@users.noreply.github.com>

--- tuc.py
+++ tuc.py
@@ -1,18 +1,33 @@
 #NaruIge
-from gettext import install
+# from gettext import install
 from selenium import webdriver
 import random
+from webdriver_manager.chrome import ChromeDriverManager
+ 
+url = "https://docs.google.com/forms/d/e/1FAIpQLScH-cTd8LzNcV-lPZZFuEWX3kAcoI5I35ZZRgdc6EmDj69CvA/viewform?usp=pp_url&"
 
-#pip install selenium
-#import sys
+result_url = (url 
+        + 'entry.506827147' + '鈴木貴久' 
+        + 'entry.1957735619' + '12時〜12時20分'
+        + 'entry.2091709137' + 'A'
+        + 'entry.2095748066' + 'takahisa@tsuda.ac.jp'
+        + 'entry.2127889036' + '教員'
+        + 'entry.2129216433' + '普通')
 
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-driver = webdriver.Chrome()
+driver.get(result_url)
 
-driver.get("https://docs.google.com/forms/d/e/1FAIpQLScH-cTd8LzNcV-lPZZFuEWX3kAcoI5I35ZZRgdc6EmDj69CvA/viewform?usp=pp_url&entry.2095748066=p19106tn@gm.tsuda.ac.jp&entry.506827147=堤菜留美&entry.2127889036=学生&entry.2091709137=A&entry.1957735619=11時20分〜11時30分&entry.2129216433=普通")
+print(result_url)
 
-print("DONE")
-#driver.find_element_by_xpath("//*[@id='mG61Hd']/div[2]/div/div[3]/div[1]/div/div/span/span").click()
-driver.find_element_by_class_name("lRwqcd").click()
+submit = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div'
 
+def click(x):
+  driver.find_element_by_xpath(x).click()
+  
+click(submit)
+
+print('DONE!')
+
+driver.close()
 driver.quit()
