17832497d6b02fade1aaab975f060e08582d1e91
tokyo121 <p19026is@gm.tsuda.ac.jp>
2022-05-21 02:05:29 +0900

    master

--- tuc.py
+++ tuc.py
@@ -1,5 +1,6 @@
 #NaruIge
 import time
+import requests
 from selenium import webdriver
 
 from webdriver_manager.chrome import ChromeDriverManager
@@ -11,7 +12,8 @@ info_url = (url
         + '&entry.1957735619=' + '12時〜12時20分'
         + '&entry.2091709137=' + 'A'
         + '&entry.2095748066=' + 'takahisa@tsuda.ac.jp'
-        + '&entry.2127889036=' + '教員')
+        + '&entry.2127889036=' + '教員'
+        + '&entry.2129216433=' + '普通')
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
@@ -19,24 +21,18 @@ driver.get(info_url)
 
 print(info_url)
 
-next_btn = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div'
-
 def click(x):
   driver.find_element_by_xpath(x).click()
 
-click(next_btn)
-
-result_url = (info_url + '&entry.2129216433=' + '普通')
-
-driver.get(result_url)
-print(result_url)
-
+next_btn = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div'
 submit = '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div[2]'
 
+click(next_btn)
 click(submit)
 
 print('DONE!')
 
+time.sleep(10)
 
-# driver.close()
-# driver.quit()
+driver.close()
+driver.quit()
