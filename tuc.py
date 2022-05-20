7df79011735b3e7d2a70b22842ec00b4c8cc5f09
tokyo121 <p19026is@gm.tsuda.ac.jp>
2022-05-21 02:57:47 +0900

    master

--- tuc.py
+++ tuc.py
@@ -8,16 +8,16 @@ from webdriver_manager.chrome import ChromeDriverManager
  
 driver = webdriver.Chrome(ChromeDriverManager().install())
 
-url = 'https://docs.google.com/forms/d/e/1FAIpQLScH-cTd8LzNcV-lPZZFuEWX3kAcoI5I35ZZRgdc6EmDj69CvA/viewform?usp=pp_url'
+url = 'https://docs.google.com/forms/d/e/1FAIpQLScH-cTd8LzNcV-lPZZFuEWX3kAcoI5I35ZZRgdc6EmDj69CvA/viewform'
 driver.get(url)
 
-menu = find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[1]/label/div/div[2]/div/span')
-print(menu)
+menu = driver.find_element_by_xpath('//*[@id="mG61Hd"]/div[2]/div/div[2]/div[4]/div/div/div[2]/div[1]/div/span/div/div[1]/label/div/div[2]/div/span')
+print(menu.text)
 
-info_url = (url 
+info_url = (url + '?usp=pp_url'
         + '&entry.506827147=' + '鈴木貴久' 
         + '&entry.1957735619=' + '12時〜12時20分'
-        + '&entry.2091709137=' + menu
+        + '&entry.2091709137=' + menu.text
         + '&entry.2095748066=' + 'takahisa@tsuda.ac.jp'
         + '&entry.2127889036=' + '教員'
         + '&entry.2129216433=' + '普通')
